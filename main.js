16a5feb356fbcd7f9cae7bc3a659718d779b9b9c
Imee Cuison <imeetwelve@gmail.com>
2015-01-27 23:29:27 -0500

    Idk

---
+++ main.js
@@ -0,0 +1,15 @@
+//sideBarGithub : function (githubData){
+//  var githubs = githubData.data.children;
+//  var markup = "";
+
+  //var sideBarTmpl = templates.sidebar;
+
+  //githubs.forEach(function(user,index,array){
+//    markup += sideBarTmpl(user);
+
+
+//  });
+
+  //$("aside").append(markup);
+
+//}
---
+++ template.js
@@ -0,0 +1,7 @@
+//templates.sidebar = [
+//  "<img src=<%= image %>/>",
+//  "<h2><%= user.name %></h2>",
+//  "<h3><%= user.login %<>/h3>",
+//  "<h4><%= user.created_at %</h4>",
+
+//].join("");
